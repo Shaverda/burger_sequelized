47ca5252a4477019d066b58bd76d390c6c462ce5
Shaverda <shaverda@gmail.com>
2017-02-15 13:33:21 -0600

    created pre-sequelized project

---
+++ controllers/burger_controller.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var colors = require('colors');
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject.red);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res) {
+  burger.create([
+    "name"
+  ], [
+    req.body.name
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    eaten: req.body.eaten
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+router.delete("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function() {
+    res.redirect("/");
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,29 @@
+var orm = require("../config/orm.js");
+
+var table_name = "hamburger";
+var burger = {
+  all: function(cb) {
+    orm.all(table_name, function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create(table_name, cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update(table_name, objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    orm.delete(table_name, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
